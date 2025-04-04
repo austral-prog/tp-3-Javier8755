873dad2ff41e889f2bcba5e3e51224ce78e7a5c8
Javier8755 <jhermidallavallol@mail.austral.edu.ar>
2025-04-04 12:42:29 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,6 +3,6 @@ def slice_simple():
    
     print(texto[0:3].lower()) # awe
     print(texto[2:5].lower()) # eso
-    print(texto.lower()) # awesome
+    print(texto[0:4].lower() + texto[4:7].lower()) # awesome
 
 slice_simple()
