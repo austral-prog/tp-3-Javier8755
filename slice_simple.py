801b9734965205233e3e7902d94a85d2ec4384b9
Javier8755 <jhermidallavallol@mail.austral.edu.ar>
2025-07-11 09:12:07 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,6 @@
 def slice_simple():
-    texto = "Awesome"
-   
+    texto="Awesome"
     print(texto[0:3].lower()) # awe
     print(texto[2:5].lower()) # eso
     print(texto[:4].lower() + texto[4:].lower()) # awesome
-
 slice_simple()
